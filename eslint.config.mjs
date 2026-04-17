8cd5841fa74b177a1662a515c471e446250a2a8b
Tamir Abutbul <1tamir198@gmail.com>
2026-04-17 14:19:58 +0300

    infra work

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,7 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
     },
-  }
+  },
 );
