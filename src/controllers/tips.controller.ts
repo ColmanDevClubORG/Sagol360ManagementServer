a5c567d7ebf73db1454d105ae30d3f97c1d33139
Lavi2910 <almog.lavi29@gmail.com>
2026-04-21 19:37:08 +0300

    fixed review comments

--- src/controllers/tips.controller.ts
+++ src/controllers/tips.controller.ts
@@ -1,14 +1,19 @@
 import { Request, Response, NextFunction } from 'express';
-import { getTip as serviceGetTip } from '../services/tips.service';
+import { StatusCodes } from 'http-status-codes';
+import { getTip as serviceGetTip } from '@/services/tips.service';
 
-export const getTip = (req: Request, res: Response, _next: NextFunction) => {
+export const getTip = (
+  { query: { totalProtocolTreatments, currentTreatmentNumber } }: Request,
+  res: Response,
+  _next: NextFunction,
+) => {
   try {
-    const totalTreatments = Number(req.query.totalProtocolTreatments);
-    const currentTreatment = Number(req.query.currentTreatmentNumber);
+    const totalTreatments = Number(totalProtocolTreatments);
+    const currentTreatment = Number(currentTreatmentNumber);
     const tip = serviceGetTip(totalTreatments, currentTreatment);
-    res.status(200).json({ tip });
+    res.status(StatusCodes.OK).json({ tip });
   } catch {
-    res.status(500).json({
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       message: 'Something went wrong',
       tip: 'יש להקפיד על שתיית מים בכמות מספקת',
     });
--- src/services/tips.service.ts
+++ src/services/tips.service.ts
@@ -1,10 +1,11 @@
 import tips from '../db/tips.json';
+import { TOTAL_TREATMENTS_20, TOTAL_TREATMENTS_60 } from '@/constants/tips.constants';
 
 export const getTip = (totalTreatments: number, currentTreatment: number): string => {
-  if (totalTreatments === 60 && currentTreatment <= 60) {
+  if (totalTreatments === TOTAL_TREATMENTS_60 && currentTreatment <= TOTAL_TREATMENTS_60) {
     return tips[currentTreatment - 1].tip;
   }
-  if (totalTreatments === 20 && currentTreatment <= 20) {
+  if (totalTreatments === TOTAL_TREATMENTS_20 && currentTreatment <= TOTAL_TREATMENTS_20) {
     return tips[(currentTreatment - 1) * 3].tip;
   }
   throw new Error('Invalid treatment parameters');
