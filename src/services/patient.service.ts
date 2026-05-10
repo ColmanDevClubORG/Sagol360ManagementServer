b3fb125fc820b0f0b597c5b0c7ea53b582320dc8
Lavi2910 <almog.lavi29@gmail.com>
2026-05-10 20:58:42 +0300

    get user back

--- src/services/patient.service.ts
+++ src/services/patient.service.ts
@@ -1,12 +1,8 @@
-import { promises as fs } from 'fs';
-import path from 'path';
 import { PATIENT_ERROR_MESSAGES } from '../constants/patient/patient.constants';
+import { patientModel } from '../models/patientModel.mongoose';
 import type { Patient } from '../models/patientModel.mongoose';
 import { appointmentsService } from './appointments.service';
-
-interface PatientsDb {
-  patients: Patient[];
-}
+import { dbService } from './db.service';
 
 interface PatientResponse {
   patientId: string;
@@ -15,18 +11,12 @@ interface PatientResponse {
   currentTreatmentNumber: number;
 }
 
-const patientsDbPath = path.resolve(process.cwd(), 'src', 'db', 'patients.json');
-
 const getAllPatients = async (): Promise<Patient[]> => {
-  const rawPatients = await fs.readFile(patientsDbPath, 'utf-8');
-  const { patients } = JSON.parse(rawPatients) as PatientsDb;
-
-  return patients;
+  return dbService.get(patientModel);
 };
 
 const getPatientByPatientId = async (patientId: string): Promise<PatientResponse> => {
-  const patients = await getAllPatients();
-  const patient = patients.find((currentPatient) => currentPatient.patientId === patientId);
+  const [patient] = await dbService.get(patientModel, { patientId });
 
   if (!patient) {
     throw new Error(PATIENT_ERROR_MESSAGES.PATIENT_NOT_FOUND);
